bcbe1124da026072746efeeb3d39d68d88e35c73
Pravinbhattarai123 <bhattaraipravin455@gmail.com>
2025-09-19 13:59:22 +0530

    Innitial commit

--- back-end/connectMongoDb.js
+++ back-end/connectMongoDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => { 
     try {
-        await mongoose.connect("mongodb+srv://ayushking6395_db_user:YaBgKr9XGH4CKmJX@cluster0.qd3u2pr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://bhattaraipravin455_db_user:U06OZqtu8H1EuQ5o@cluster0.xy9webj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB connected");
     } catch (error) {
         console.error("Error connecting to MongoDB: ", error);
